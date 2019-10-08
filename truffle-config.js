6589b2dbf9622829e03622cd26457e3c281003bc
Mizuki Zenta <mizukisb@gmail.com>
2019-10-09 10:09:33 +1100

    added a ropsthen conf

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var infura_apikey = "a5730bd983fb4785b9bab40045b80866";
+var mnemonic = "coral thunder dash quote word usual permit lazy laptop erase sting lady";
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,6 +10,10 @@ module.exports = {
   networks: {
     develop: {
       port: 7545
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/" + infura_apikey),
+      network_id: 3
     }
   }
 };
